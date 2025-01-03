6fc64e7e5d4a7ccdf826b7b56ced01887d91dbe4
ritika-abc <ritikasss321@gmail.com>
2025-01-03 18:27:01 +0530

    load

---
+++ about.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Averath Agricos LLP</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Averath Agricos LLP" name="keywords">
+    <meta content="Averath Agricos LLP" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Red+Rose:wght@600;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    include "header.php";
+
+    ?>
+
+
+
+    <div class="container-fluid" style="background-color: #a3ba8d; margin-top:-35px">
+        <div class="row g-0 justify-content-center">
+            <div class=" col-lg-6">
+                <img src="image/about.jpg" height="auto" width="100%" alt="">
+            </div>
+            <div class="col-lg-6 align-self-center">
+                <div class="container text-center py-5 mt-4">
+                    <h1 class="display-5 text-dark mb-3 animated slideInDown">About Us</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about us -->
+    <section style="margin: 4rem 0rem;">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content_about">
+                        <h3>Welcome to <b class="text-primary"> Averath Agricos LLP</b></h3>
+                        <p>At Palmira, we are passionate about creating a positive environmental impact, and our commitment goes beyond just producing products. We envision a world where sustainability is at the core of every decision, and we strive to lead by example in the movement toward a cleaner, greener future. Our manufacturing unit, which operates with a deep respect for nature, focuses on harnessing the power of palm leaves—a renewable resource that is both abundant and biodegradable.
+                        </p>
+                        <p>We believe that the key to a sustainable future lies in the choices we make today. Our products are carefully crafted to replace traditional plastic and other non-biodegradable materials, which contribute significantly to environmental pollution. By utilizing palm leaves, a natural byproduct of palm tree cultivation, we are able to reduce waste while creating products that are both functional and eco-friendly. Our range includes biodegradable plates, cutlery, and packaging solutions—all designed to offer an eco-conscious alternative to the harmful materials that are typically used in disposable products.</p>
+
+                        <p>Our process prioritizes minimal environmental impact at every step. From sourcing raw materials locally to ensuring that our production methods are energy-efficient and waste-reducing, we are committed to implementing best practices in sustainability. We work closely with our partners and local communities to promote awareness about the importance of adopting greener lifestyles, and we are dedicated to making sustainable choices accessible to everyone.</p>
+
+
+                        <p>At Palmira, we recognize that every small action can contribute to a bigger change. Whether it’s a small café choosing our palm leaf plates or a large event opting for biodegradable cutlery, we’re proud to be part of the solution. Our products are designed to be durable, compostable, and responsibly produced, ensuring that they leave behind no lasting footprint on the planet.</p>
+
+                        <p>We invite you to join us in our mission to reduce plastic waste and make a lasting impact on the environment. Together, we can create a pollution-free world for future generations—one palm leaf at a time. Let’s embrace sustainability and work hand-in-hand towards a greener tomorrow.</p>
+
+                        <hr>
+                        <h6>Our Mission</h6>
+                        <p>At Palmira, our mission is to create sustainable, eco-friendly alternatives to single-use plastics and other harmful materials, empowering individuals and businesses to make responsible choices for the environment. By harnessing the natural power of palm leaves, we aim to produce high-quality, biodegradable products that help reduce pollution and promote a cleaner, greener world.</p>
+                        <p>We are dedicated to advancing environmental responsibility through every step of our production process— from sourcing renewable materials to minimizing our ecological footprint— all while offering innovative solutions that support a sustainable, pollution-free future. Our mission is to inspire positive change, one palm leaf at a time.
+
+
+
+                        </p>
+                        <!-- <a href="certificate.php" class="btn btn-primary px-3">View Our Certificate</a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Features Start -->
+    <style>
+        .feature1 {
+            background-image: linear-gradient(rgba(3, 27, 78, .3), rgba(3, 27, 78, .3)), url(image/ativment.jpg);
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+   
+    <!-- Features End -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <table class="table table-hover table-bordered">
+                    <tr>
+                        <th>Company Name</th>
+                        <td>Palmira</td>
+                    </tr>
+                    <tr>
+                        <th>CEO Name</th>
+                        <td>Prashant Chanddraman</td>
+                    </tr>
+                    <tr>
+                        <th>Year of Establishment</th>
+                        <td>2024</td>
+                    </tr>
+                    <tr>
+                        <th>Nature of Service</th>
+                        <td>Exportrs and Domestic Suppliers</td>
+                    </tr>
+                    <tr>
+                        <th>Company Address</th>
+                        <td>old road anangoor , opposite  NSS club anangoor ,Kerala , India-671121</td>
+                    </tr>
+                    <tr>
+                        <th>Contact Number</th>
+                        <td><a href="tel:9074108483">+91 9074108483</a> </a></td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td><a href="mailto:Prashanthbkky@gmail.com">Prashanthbkky@gmail.com</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include "footer.php";
+
+    ?>
---
+++ footer.php
@@ -0,0 +1,89 @@
+
+    <!-- Footer Start -->
+    <div class="container-fluid footer position-relative bg-dark text-white-50 py-5 wow fadeIn" data-wow-delay="0.1s"
+        style="background-color: black !important;">
+        <div class="container">
+            <div class="row g-5 py-5">
+                <div class="col-lg-5 pe-lg-4">
+                    <a href="index.html" class="navbar-brand">
+                        <img src="image/logo/logo.jpg" class="rounded mb-1" height="100px" width="200px" alt="">
+                    </a>
+                    <p class="fs-5 mb-4">At Palmira, we are driven by a clear and unwavering vision to contribute to a cleaner, greener world. As part of our commitment to sustainability,  <a href=""><u>Read
+                                More</u></a></p>
+
+                    <div class="d-flex mt-4">
+                        <a class="btn btn-lg-square btn-primary me-2" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-lg-square btn-primary me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-lg-square btn-primary me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-lg-square btn-primary me-2" href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30573.660014756053!2d74.7014948018882!3d16.691514138790982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc0d89361430827%3A0xfb62321db4d960fa!2sKagwad%2C%20Karnataka%20591223!5e0!3m2!1sen!2sin!4v1727349866142!5m2!1sen!2sin" class="rounded border border-white mt-2 shadow-lg p-1" width="200px" height="130px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                </div>
+                <div class="col-lg-7 ps-lg-5">
+                    <div class="row g-5">
+                        <div class="col-sm-6">
+                            <h4 class="text-light mb-4">Contact Now</h4>
+                            <p><i class="fa fa-map-marker-alt me-2"></i>Kerala</p>
+                            <p><i class="fa fa-phone-alt me-2"></i>+91 9074108483</p>
+                            <!-- <p><i class="fa fa-phone-alt me-2"></i>+91 9503433176</p> -->
+                            <p><i class="fa fa-envelope me-2"></i>averathagricosllp@gmail.com</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <h4 class="text-light mb-4">Popular Links</h4>
+                            <a class="btn btn-link" href=".php">About Us</a>
+                            <a class="btn btn-link" href=".php">Contact Us</a>
+                            <a class="btn btn-link" href=".php">Our Products</a>
+                            <a class="btn btn-link" href=".php">Our Services</a>
+                          
+                        </div>
+                        <div class="col-sm-12 text-start text-md-end">
+                            <a href="https://web2export.com" target="_blank">
+                                <img src="image/logo/webexport.jpg" height="80px" width="210px" class="p-1 rounded"
+                                    alt="">
+
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright bg-dark text-white-50 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start">
+                    <p class="mb-0">&copy; <a href="/">PALMIRA</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    <p class="mb-0">Designed by <a href="https://web2export.com" target="_blank">web2export.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
+            class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
